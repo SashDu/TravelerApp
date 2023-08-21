cda136e10dfb740dd8f4764d8ee5ab20ccf11665
SashDu <sashadubina1996@gmail.com>
2023-08-21 21:48:21 +0300

    created some logic for authentication

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+    #old url for entrance
+    path('login/', views.user_login, name='login'),
+    # url for entrance and exit
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('', views.dashboard, name='dashboard'),
+]
