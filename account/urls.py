d468c72e1a59bf28971ed52861793b0a0788818b
SashDu <sashadubina1996@gmail.com>
2023-08-23 02:38:50 +0300

    added some templates for reset and change password

--- account/urls.py
+++ account/urls.py
@@ -5,10 +5,32 @@ from . import views
 
 urlpatterns = [
     #old url for entrance
-    path('login/', views.user_login, name='login'),
+    #path('login/', views.user_login, name='login'),
     # url for entrance and exit
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    #url for change password
+    path('password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password-change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    #url for password reset
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 
     path('', views.dashboard, name='dashboard'),
 ]
